40162cbcf41101b1c336f88f5ea2881dbdd61551
David THENON <sveetch@gmail.com>
2016-05-05 09:39:50 +0200

    Modified conf/pages loading to raise exceptions that is useful to debug, but not for the finding module part since exception is about imp.find_module that is useless (critical message about not finding module is enough), bump to 0.7.2

--- optimus/__init__.py
+++ optimus/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-A simple building environment to produce static HTML from Jinja templates and 
+A simple building environment to produce static HTML from Jinja templates and
 with assets compress managing with webassets
 """
-__version__ = "0.7.1"
+__version__ = "0.7.2"
--- optimus/conf.py
+++ optimus/conf.py
@@ -8,11 +8,11 @@ ENVIRONMENT_VARIABLE = "OPTIMUS_SETTINGS_MODULE"
 
 def import_settings(name=None):
     """
-    Load the settings, use "os.environ" to find the settings module name if "name" 
+    Load the settings, use "os.environ" to find the settings module name if "name"
     argument is not given
     """
     logger = logging.getLogger('optimus')
-    
+
     if name is None:
         # Stealed from "django.conf"
         try:
@@ -23,9 +23,9 @@ def import_settings(name=None):
             # NOTE: This is arguably an EnvironmentError, but that causes
             # problems with Python's interactive help.
             raise ImportError("Settings cannot be imported, because environment variable %s is undefined." % ENVIRONMENT_VARIABLE)
-    
+
     _settings = import_settings_module(name)
-    
+
     # Raise exception if these required settings are not defined
     required_settings = ('DEBUG','SITE_NAME','SITE_DOMAIN','SOURCES_DIR','TEMPLATES_DIR','PUBLISH_DIR','STATIC_DIR','STATIC_URL',)
     missing_settings = []
@@ -35,13 +35,13 @@ def import_settings(name=None):
     if len(missing_settings)>0:
         logger.error("The following settings are required but not defined in the used settings file: {0}".format(", ".join(missing_settings)))
         raise NameError
-    
+
     # Fill default required settings
-    
+
     # The directory where webassets will store his cache
     if not hasattr(_settings, "WEBASSETS_CACHE"):
         setattr(_settings, "WEBASSETS_CACHE", os.path.join(_settings.PROJECT_DIR, '.webassets-cache'))
-    
+
     # Bundles
     if hasattr(_settings, "EXTRA_BUNDLES"):
         setattr(_settings, "BUNDLES", _settings.EXTRA_BUNDLES)
@@ -50,7 +50,7 @@ def import_settings(name=None):
         setattr(_settings, "BUNDLES", {})
     if not hasattr(_settings, "ENABLED_BUNDLES"):
         setattr(_settings, "ENABLED_BUNDLES", _settings.BUNDLES.keys())
-    
+
     # ReSTructuredText parser settings to use when building a RST document
     if not hasattr(_settings, "RST_PARSER_SETTINGS"):
         setattr(_settings, "RST_PARSER_SETTINGS", {
@@ -60,7 +60,7 @@ def import_settings(name=None):
             'footnote_references': 'superscript',
             'doctitle_xform': False,
         })
-    
+
     # Default directory for translation catalog
     if not hasattr(_settings, "LOCALES_DIR"):
         setattr(_settings, "LOCALES_DIR", os.path.join(_settings.PROJECT_DIR, 'locale'))
@@ -86,24 +86,24 @@ def import_settings(name=None):
         })
     if not hasattr(_settings, "I18N_EXTRACT_SOURCES"):
         setattr(_settings, "I18N_EXTRACT_SOURCES", (_settings.PROJECT_DIR,))
-    
+
     # Python paths for each extensions to use with Jinja2
     if not hasattr(_settings, "JINJA_EXTENSIONS"):
         setattr(_settings, "JINJA_EXTENSIONS", (
             'jinja2.ext.i18n',
         ))
-    
+
     # Python path to the file that contains pages map, this is relative to your project
     if not hasattr(_settings, "PAGES_MAP"):
         setattr(_settings, "PAGES_MAP", "pages")
-    
+
     # Sources files or directory to synchronize within the static directory
     if not hasattr(_settings, "FILES_TO_SYNC"):
         setattr(_settings, "FILES_TO_SYNC", ())
-    
-    # These are the default watcher settings, you can customize them if you want, uncomment 
+
+    # These are the default watcher settings, you can customize them if you want, uncomment
     # parts you want to change, usually you'll change only the "pattern" values
-    # You don't need to uncomment this if you want to use the watcher with these default 
+    # You don't need to uncomment this if you want to use the watcher with these default
     # parameters
 
     # Templates watcher settings
@@ -114,20 +114,20 @@ def import_settings(name=None):
         setattr(_settings, "WATCHER_ASSETS_PATTERNS", {})
 
     return _settings
-    
 
-def import_project_module(name, finding_module_err='Unable to find module: {0}', 
+
+def import_project_module(name, finding_module_err='Unable to find module: {0}',
                                 import_module_err='Unable to load module: {0}'):
     """
-    Load the given module name, only from the current directory (where the CLI has been 
+    Load the given module name, only from the current directory (where the CLI has been
     launched)
     """
     project_directory = os.getcwd()
-    
+
     logger = logging.getLogger('optimus')
     logger.info('Loading "%s" module', name)
     logger.info('Module searched in: %s', project_directory)
-    
+
     # Add the project to the sys.path
     project_name = os.path.basename( os.path.abspath( project_directory ) )
     sys.path.append( os.path.normpath( os.path.join(project_directory, '..') ) )
@@ -135,31 +135,32 @@ def import_project_module(name, finding_module_err='Unable to find module: {0}',
     try:
         project_module = __import__(project_name, '', '', [''])
     except ImportError:
-        logger.critical("Unable to load the project named '{0}'".format(project_name))
+        logger.critical("Unable to load project named '{0}'".format(project_name))
         raise
     # Cleanup the sys.path of the project path
     sys.path.pop()
-    
+
+    fp = pathname = description = None
     try:
         fp, pathname, description = imp.find_module(name, [project_directory])
+    except ImportError:
+        logger.critical(finding_module_err.format(name))
+        # dont raising exception that is not really helping since it point out
+        # to 'imp.find_module' line
+        #raise
+        sys.exit()
+    else:
         try:
             settings = imp.load_module(name, fp, pathname, description)
         except:
             logger.critical(import_module_err.format(name))
-            # TODO: print out the exception without to use "raise" because it bubble up, 
-            # catched by upper try..except and so display 'finding' AND 'import' errors
-            # The exception is so usefull to see when the module contains error (like 
-            # syntax error..)
+            # Print out the exception because its so useful to debug
+            raise
             sys.exit()
-        finally:
-            # Close fp explicitly.
-            if fp:
-                fp.close()
-    except ImportError:
-        logger.critical(finding_module_err.format(name))
-        # TODO: print out the exception
-        raise
-        sys.exit()
+    finally:
+        # Close fp explicitly.
+        if fp:
+            fp.close()
 
     return settings
 
@@ -171,6 +172,6 @@ def import_settings_module(name):
 def import_pages_module(name):
     """Helper to use a distinct error label when loading page module"""
     return import_project_module(name, finding_module_err='Unable to find pages module: {0}',
-                                       import_module_err='Unable to load settings module, it probably have errors: {0}')
+                                       import_module_err='Unable to load pages module, it probably have errors: {0}')
 
 settings = import_settings()
