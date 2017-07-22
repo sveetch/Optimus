389e1de685b776c4f80e7e73dec65406aa9f898b
David THENON <sveetch@gmail.com>
2017-07-22 21:58:46 +0200

    Configured tests structure and tox

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+"""
+Some fixture methods
+"""
+import os
+import pytest
+
+#@pytest.fixture(scope='session')
+#def temp_builds_dir(tmpdir_factory):
+    #"""Prepare a temporary build directory"""
+    #fn = tmpdir_factory.mktemp('builds')
+    #return fn
+
+
+#@pytest.fixture(scope="module")
+#def settings():
+    #"""Initialize and return settings (mostly paths) for fixtures (scope at module level)"""
+    #return FixturesSettingsTestMixin()
