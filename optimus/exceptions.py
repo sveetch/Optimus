21803670f3bb5f95116a189d0588269fe79217d4
David THENON <sveetch@gmail.com>
2017-07-31 01:53:08 +0200

    Added missing files

---
+++ optimus/exceptions.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Exceptions
+==========
+
+Specific exceptions that Optimus code can raise.
+"""
+
+
+class OptimusBaseException(Exception):
+    """
+    Base for Optimus exceptions.
+    """
+    pass
+
+
+class DestinationExists(OptimusBaseException):
+    """
+    Exception to be raised when a destination allready exists for a new project
+    to create
+    """
+    pass
+
+
+class TemplateImportError(OptimusBaseException):
+    """
+    Exception to be raised when a template module import fails.
+    """
+    pass
---
+++ tests/03_start_project/03_get_template_module.py
@@ -0,0 +1,28 @@
+import os
+import io
+import logging
+
+import pytest
+
+from optimus.exceptions import TemplateImportError
+from optimus.start_project import ProjectStarter
+
+
+def test_template_module_success():
+    """
+    Succeed to import basic template module
+    """
+    starter = ProjectStarter("foo", "bar")
+    mod = starter.get_template_module('optimus.samples.basic')
+
+    assert mod.SOURCES_FROM == 'sources'
+
+
+def test_template_module_fail():
+    """
+    Destination allready exists, fail
+    """
+    starter = ProjectStarter("foo", "bar")
+
+    with pytest.raises(TemplateImportError):
+        starter.get_template_module('optimus.samples.idontexist')
